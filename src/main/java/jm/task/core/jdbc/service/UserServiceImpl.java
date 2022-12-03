603885ac821177f2c2c8317e257c9bca39742c19
Andrey <KhalezinAndrey89@gmail.com>
2022-12-03 21:36:56 +0300

    Задача 1.1.4 version 4

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -6,7 +6,8 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
 
     public void createUsersTable() {
         userDaoJDBC.createUsersTable();
